7b05cb6eb504d724e46bb9f337f6816c6f4b1913
Elodie Orozco Taveira <elodie.orotav@hotmail.com>
2026-08-03 15:57:52 +0200

    fix: correction migration db laptop

--- backend/drizzle/0001_20260630123624_lazy_thunderbird.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "activities" DROP CONSTRAINT "activities_users_id_users_id_fkey";--> statement-breakpoint
-ALTER TABLE "activities" DROP COLUMN "users_id";
--- backend/drizzle/uniqueConversationPerActivity/migration.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "conversations" ADD CONSTRAINT "conversations_activity_id_unique" UNIQUE("activity_id");
